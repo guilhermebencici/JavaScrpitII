dd06776a7e3c7f8f9d479645c6ad157204aec8c8
Guilherme Bencici <guilhermebencici@gmail.com>
2021-12-16 21:39:50 -0300

    Commit aula 16/12

--- index.js
+++ index.js
@@ -1,12 +1,55 @@
 // function carregamentoPagina() {
 //     alert("Mensagem apresentada no carregamento do sistema");
-// }
+
+
+function populaTabela() {
+    if (localStorage.getItem("produtos")) {
+        let produtos = JSON.parse(localStorage.getItem("produtos"))
+
+        $("#tblProdutos tbody").html("")
+        produtos.forEach((produto) => {
+            $("#tblProdutos tbody").append(`<tr>
+                <td>${produto.nome}</td>
+                <td>${produto.quantidade}</td>
+                <td>${produto.valor}</td>
+                <td style="width: 50px"><button type="button" class="btn btn-info">E</button></td>
+                <td style="width: 50px"><button type="button" class="btn btn-danger">D</button></td>
+            </tr>`)
+        })
+
+    }
+}
 
 $(() => {
     //Código executado no carregamento da página
-    // alert("Mensagem apresentada no carregamento do sistema");
+
+    populaTabela()
 
     $("#btnSalvar").click(() => {
-        console.log("FUNÇÃO CLICK FUNCIONOU");
+        // "localStorarge, guardara as informações"
+
+        let produto = {}
+
+        produto.nome = $("#nmProduto").val()
+        produto.quantidade = $("#quantidade").val()
+        produto.valor = $("#vlrProduto").val()
+
+        let listaProdutos = []
+        if (localStorage.getItem("produtos")) {
+            listaProdutos = JSON.parse(localStorage.getItem("produtos"))
+        }
+
+        listaProdutos.push(produto) // O método push() adiciona  elementos ao final de um array
+
+        localStorage.setItem("produtos", JSON.stringify(listaProdutos));
+
+        alert("Produto salvo com sucesso");
+
+        $("#nmProduto").val("");
+        $("#quantidade").val("");
+        $("#vlrProduto").val("");
+
+        populaTabela()
+
     })
 })
